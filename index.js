7e5cb05c914153843f29f99b3e0ed20a3df0fb2d
decibe1magic <decibe1magic@gmail.com>
2021-07-15 11:48:07 +0900

    2nd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like SeungWon!');
+    res.send('I love SeungWon!');
 });
 
 app.get('/start', function(req, res){
